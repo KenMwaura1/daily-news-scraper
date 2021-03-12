39454760996ba3a9c244c0b3012c13cced075815
KenMwaura1 <kemwaura@gmail.com>
2021-03-12 16:53:14 +0300

    updated the README

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # daily-news-scraper
 
 Ths is a simple web scraper utilizing [newspaper3k](https://newspaper.readthedocs.io/en/latest/user_guide/quickstart.html#) to scrape news articles and send them via text.
+
+## Prerequisites 
+This project also uses an .env file to store the API key and username.
+Both can be obtained by [signing up/logging into Africas Talking](https://www.account.africastalking.com/)
+An env example is provided for reference 
--- news_scraper.py
+++ news_scraper.py
@@ -1,7 +1,17 @@
-from newspaper import Article
+import os
+import time
 import newspaper
 import africastalking as at
 from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv('api_key')
+username = os.getenv('username')
+mobile_number = os.getenv('mobile_number')
+
+at.initialize(username, api_key)
+sms = at.SMS
 
 business_daily = "https://www.businessdailyafrica.com/"
 standard_daily = "https://www.standardmedia.co.ke/"
@@ -13,7 +23,7 @@ def top_news(url):
     # get top articles on standard standard
     news_source = newspaper.build(url, memoize_articles=False)
     top_articles = []
-    for index in range(5):
+    for index in range(3):
         article = news_source.articles[index]
         article.download()
         article.parse()
@@ -28,3 +38,10 @@ top_news(business_daily)
 top_news(standard_daily)
 
 print(message)
+
+
+def send_message(news: list, number: int):
+    sms.send(news, [number])
+
+
+send_message(str(message), mobile_number)

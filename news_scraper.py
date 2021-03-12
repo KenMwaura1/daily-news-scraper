6e6bb229d4f52bf00a24052d4dc8f3e28271a3aa
KenMwaura1 <kemwaura@gmail.com>
2021-03-12 15:45:25 +0300

    added africas talking and a message variable

--- news_scraper.py
+++ news_scraper.py
@@ -1,23 +1,17 @@
 from newspaper import Article
 import newspaper
+import africastalking as at
+from dotenv import load_dotenv
 
-""" 
-url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
-url2 = "https://nation.africa/kenya"
-article = Article(url2)
-article.download()
-article.parse()
-print(article.text)
-print(article.text)
-"""
-bdaily = newspaper.build("https://www.businessdailyafrica.com/bd/corporate/technology", memoize_articles=False)
-daily_nation = newspaper.build("https://nation.africa/kenya/news/tech", memoize_articles=False)
-sdaily = newspaper.build("https://www.standardmedia.co.ke/", memoize_articles=False)
+business_daily = "https://www.businessdailyafrica.com/"
+standard_daily = "https://www.standardmedia.co.ke/"
+
+message = []
 
 
 def top_news(url):
     # get top articles on standard standard
-    news_source = newspaper.build(url,memoize_articles=False)
+    news_source = newspaper.build(url, memoize_articles=False)
     top_articles = []
     for index in range(5):
         article = news_source.articles[index]
@@ -26,10 +20,11 @@ def top_news(url):
         top_articles.append(article)
     for a in top_articles:
         print(a.title, a.url)
+        message.append(a.title)
+        message.append(a.url)
 
 
-sample = bdaily.articles[2]
-sample.download()
-sample.parse()
-print(sample.title)
-print(sample.url)
+top_news(business_daily)
+top_news(standard_daily)
+
+print(message)

7e492a6ac652978f44ef270dc0ce8b76e4e82b19
KenMwaura1 <kemwaura@gmail.com>
2021-03-19 12:43:55 +0300

    added url shortening

--- news_scraper.py
+++ news_scraper.py
@@ -1,6 +1,7 @@
 import os
 import newspaper
 import africastalking as at
+import bitlyshortener as bts
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,10 +9,14 @@ load_dotenv()
 api_key = os.getenv('api_key')
 username = os.getenv('username')
 mobile_number = os.getenv('mobile_number')
-# Initialize the Africas talking client using username and api api_key
+# Initialize the Africas talking client using username and api_key
 at.initialize(username, api_key)
 # create a variable to reference the SMS client
 sms = at.SMS
+# Get the token(s) and create a shortener variable
+token = os.getenv('bitly_token')
+print(token)
+shortener = bts.Shortener(tokens=[token], max_cache_size=256)
 
 # create variables to hold urls to be scraped
 business_daily = "https://www.businessdailyafrica.com/"
@@ -19,12 +24,13 @@ standard_daily = "https://www.standardmedia.co.ke/"
 # create an empty list to hold the headlines and urls
 message = []
 
-
+long_urls = []
 # Create a function to scrape the top 3 headlines from news sources
 def top_news(url):
     # get top articles on standard standard
-    news_source = newspaper.build(url,)
+    news_source = newspaper.build(url, memoize_articles=False)
     top_articles = []
+
     for index in range(3):
         article = news_source.articles[index]
         article.download()
@@ -32,13 +38,19 @@ def top_news(url):
         top_articles.append(article)
     for a in top_articles:
         print(a.title, a.url)
+        long_urls.append(a.url)
+        short_url = shortener.shorten_urls([a.url])
         message.append(a.title)
-        message.append(a.url)
+        for url in short_url:
+            message.append(url)
+        # message.append(a.url)
 
 
 top_news(business_daily)
 top_news(standard_daily)
 
+
+print(message)
 
 # Create a function to send a message containing the scraped news headlines.
 def send_message(news: list, number: int):
@@ -48,6 +60,5 @@ def send_message(news: list, number: int):
     except Exception as e:
         print(f" Houston we have a problem: {e}")
 
-
 # Call the function passing the message  and mobile_number as a arguments
-send_message(str(message), mobile_number)
+# send_message(str(message), mobile_number)
